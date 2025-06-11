0c925b5383fb84af35e405645152025c720a8bae
moutb <manueloute@gmail.com>
2025-06-11 03:35:54 +0200

    fix: solved issue when deleting items in shopping cart

--- src/features/cart/modules/reducer.ts
+++ src/features/cart/modules/reducer.ts
@@ -10,16 +10,21 @@ export function cartReducer(
         case 'ADD':
             return { products: [...state.products, action.payload] };
         case 'REMOVE':
+            const indexToRemove = state.products.findIndex(
+                (product) =>
+                    product.id === action.payload.id &&
+                    product.color.name === action.payload.color.name &&
+                    product.storage.capacity ===
+                        action.payload.storage.capacity,
+            );
+
+            if (indexToRemove === -1) return state;
+
             return {
-                products: state.products.filter(
-                    (product) =>
-                        !(
-                            product.id === action.payload.id &&
-                            product.color.name === action.payload.color.name &&
-                            product.storage.capacity ===
-                                action.payload.storage.capacity
-                        ),
-                ),
+                products: [
+                    ...state.products.slice(0, indexToRemove),
+                    ...state.products.slice(indexToRemove + 1),
+                ],
             };
         default:
             return state;
