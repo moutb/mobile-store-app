96ab06ebf0880f4dea5a19d71fdb128e31a538e4
moutb <manueloute@gmail.com>
2025-06-11 03:23:33 +0200

    add: common next image jest mock

--- src/__test__/mocks/next-image.tsx
+++ src/__test__/mocks/next-image.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text, @next/next/no-img-element */
 jest.mock('next/image', () => ({
     __esModule: true,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
